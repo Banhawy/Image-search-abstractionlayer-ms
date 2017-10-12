28b2a8a3c3fd4c9f323de15578d5e3493378d4e5
Adham EL Banhawy <the.benhawy@gmail.com>
2017-10-12 14:53:29 -0500

    router succesfully calls api and returns json results

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const imgur = require('../services/imgur');
 
 router.get('/', (req,res)=>{
     res.send('hello');
 })
 
-router.get('latest', (req,res)=>{
+router.get('/latest', (req,res)=>{
 });
 
-router.get('api/imagesearch/:q', (req,res)=>{
+router.get('/api/search/:q', (req,res)=>{
+    const query = req.params.q;
+    const offset = req.params.offset;
+    imgur.getImage(query, offset).then(results=>{
+        res.json(results);
+    })
 });
 
 module.exports = router;
