f7c6a10487a786e7533dd426ecd557fb438cab6a
Adham EL Banhawy <the.benhawy@gmail.com>
2017-10-12 15:13:17 -0500

    Added insert query into History collection

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const imgur = require('../services/imgur');
+const History = require('../models/history');
 
 router.get('/', (req,res)=>{
     res.send('hello');
@@ -12,6 +13,7 @@ router.get('/latest', (req,res)=>{
 router.get('/api/search/:q', (req,res)=>{
     const query = req.params.q;
     const offset = req.params.offset;
+    new History({ term: query }).save();
     imgur.getImage(query, offset).then(results=>{
         res.json(results);
     })
